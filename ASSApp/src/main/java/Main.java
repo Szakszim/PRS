1a513c5f6e469c4eb7eae0bff20d3e7b39e9efce
Szakszim <saxim826@gmail.com>
2019-05-14 21:23:17 +0200

    Initial

--- ASSApp/src/main/java/Main.java
+++ ASSApp/src/main/java/Main.java
@@ -9,8 +9,11 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import parameters.ParameterProvider;
 
+import javax.smartcardio.*;
 import javax.swing.*;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 //
@@ -38,8 +41,21 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws CardException {
+        TerminalFactory factory = TerminalFactory.getDefault();
+        List<CardTerminal> terminals = factory.terminals().list();
+        System.out.println("Terminals: " + terminals);
 
+        CardTerminal reader = terminals.get(0);
+        Card card = reader.connect("*");
+        System.out.println(card.getATR().toString());
+
+        card.beginExclusive();
+        CardChannel channel = card.getBasicChannel();
+        ResponseAPDU resp = channel.transmit(new CommandAPDU(128, 202, 0x9f, 0x7f, 256));
+        byte[] data = resp.getData();
+        byte[] ICSerialNumber = Arrays.copyOfRange(data,15,19);
+        System.out.println(hexToString(ICSerialNumber));
         Locale.setDefault(new Locale("en", "GB"));
 
         FXMLLoader innerLoader = new FXMLLoader(getClass().getResource("views/general/LoginScreen.fxml"));
@@ -78,5 +94,15 @@ public class Main extends Application {
 
     public static void Main(String[] args) {
         launch(args);
+    }
+    public static String hexToString(final byte[] data) {
+        char[] hexValue = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int length = data.length;
+        char[] out = new char[length*2];
+        for (int i = 0, j = 0; i < length; i++) {
+            out[j++] = hexValue[(0xF0 & data[i]) >>> 4];
+            out[j++] = hexValue[0x0F & data[i]];
+        }
+        return new String(out);
     }
 }
