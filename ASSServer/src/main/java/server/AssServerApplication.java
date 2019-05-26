cff453884bb394f0822af7b105fb7473c32e359f
Kamil Zieliński <kamilziel@tlen.pl>
2019-05-26 11:16:18 +0200

    [0006] Fixed: AssServerApplication.java
    Deleted: Old imports, which caused to fail during compilation.

--- ASSServer/src/main/java/server/AssServerApplication.java
+++ ASSServer/src/main/java/server/AssServerApplication.java
@@ -1,12 +1,9 @@
 package server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import server.repositories.AddressRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "server.repositories")
